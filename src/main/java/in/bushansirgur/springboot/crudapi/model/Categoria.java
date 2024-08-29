f8a4acb9a3b7e5b28470e7f8db3a4c509a986e5d
alsoares086 <117699313+alsoares086@users.noreply.github.com>
2024-08-29 05:41:46 -0500

    enum Categoria

---
+++ src/main/java/in/bushansirgur/springboot/crudapi/model/Categoria.java
@@ -0,0 +1,12 @@
+package in.bushansirgur.springboot.crudapi.model;
+
+public enum Categoria {
+    HATCH_COMPACTO,
+    HATCH_MEDIO,
+    SEDAN_COMPACTO,
+    SEDAN_MEDIO,
+    SEDAN_GRANDE,
+    MINIVAN,
+    ESPORTIVO,
+    UTILITARIO
+}
