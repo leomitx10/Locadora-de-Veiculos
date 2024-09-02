36a00894738a9eba6374381448874954c2da8410
DanrleyS <122413762+DanrleyS@users.noreply.github.com>
2024-09-02 00:22:26 -0300

    Update ModeloCarro.java
    false

--- Minha_locadora/demo/src/main/java/com/squad2/locadoradeveiculos/model/ModeloCarro.java
+++ Minha_locadora/demo/src/main/java/com/squad2/locadoradeveiculos/model/ModeloCarro.java
@@ -12,7 +12,7 @@ public class ModeloCarro {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false, unique = false)
     private String descricao;
 
     @ManyToOne
